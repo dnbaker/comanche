65659ce35aaa215ef2fe83b0492910516b43f799
Daniel Baker <daniel.n.baker@ibm.com>
2019-07-02 10:18:49 -0700

    Further work monkey-patching.

--- src/lib/dpdk/bootstrap.sh
+++ src/lib/dpdk/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Boot-strapping DPDK ..."
+set -e
 DPDK_VERSION=dpdk-18.08
 
 if [ ! -d ./$DPDK_VERSION ] ; then
@@ -9,21 +10,31 @@ if [ ! -d ./$DPDK_VERSION ] ; then
     ln -s ./$DPDK_VERSION dpdk
 fi
 
+if [ ! -d ./dpdk ]; then
+	echo "FAIL: not present"
+	exit 1
+fi
+
 CPUTYPE=$(lscpu | awk '/Model/{if($0 ~ "POWER") print($3)}')
 if [ "$CPUTYPE" = "POWER9" ] ; then
-    cd dpdk
-      sed 's:power8:power9:g' < config/defconfig_ppc_64-power8-linuxapp-gcc > config/defconfig_ppc_64-power9-linuxapp-gcc
-      sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' mk/target/generic/rte.vars.mk
-    cd ..
-fi
-if [ "$CPUTYPE" = "POWER8" ] ; then
-    cd dpdk
-      sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' mk/target/generic/rte.vars.mk
-    cd ..
+    sed 's:power8:power9:g' dpdk/config/defconfig_ppc_64-power8-linuxapp-gcc > dpdk/config/defconfig_ppc_64-power9-linuxapp-gcc
+    sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' dpdk/mk/target/generic/rte.vars.mk
+    sed -i 's:-march=native:-mcpu=native -mtune=native:g' dpdk/mk/target/generic/rte.vars.mk
+    sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' dpdk/mk/machine/native/rte.vars.mk
+    sed -i 's:-march=native:-mcpu=native -mtune=native:g' dpdk/mk/machine/native/rte.vars.mk
+    cp  dpdk/config/defconfig_ppc_64-power9-linuxapp-gcc ./$DPDK_VERSION/config || echo "present"
+elif [ "$CPUTYPE" = "POWER8" ] ; then
+    sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' dpdk/mk/target/generic/rte.vars.mk
+    sed -i 's:-march=native:-mcpu=native -mtune=native:g' dpdk/mk/target/generic/rte.vars.mk
+    sed -i 's:-march=$(RTE_MACHINE):-mcpu=$(RTE_MACHINE) -mtune=$(RTE_MACHINE):g' dpdk/mk/machine/native/rte.vars.mk
+    sed -i 's:-march=native:-mcpu=native -mtune=native:g' dpdk/mk/machine/native/rte.vars.mk
+    cp  dpdk/config/defconfig_ppc_64-power8-linuxapp-gcc ./$DPDK_VERSION/config || echo "present2"
+else
+    echo "processing for x86-64"
+    cp defconfig_x86_64-native-linuxapp-gcc ./$DPDK_VERSION/config
 fi
 
 cp eal_memory.c ./$DPDK_VERSION/lib/librte_eal/linuxapp/eal/eal_memory.c
-cp defconfig_x86_64-native-linuxapp-gcc ./$DPDK_VERSION/config
 cp build.sh ./$DPDK_VERSION/build.sh
 
 cd ./$DPDK_VERSION/ ; ./build.sh $1
--- src/lib/dpdk/build.sh
+++ src/lib/dpdk/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+THREADS=1
+if [ $# -ge 1 ]; then
+	THREADS=$1
+fi
+
 CPUTYPE=$(lscpu | awk '/Model/{if($0 ~ "POWER") print($3)}')
 echo $CPUTYPE
 if [ "$CPUTYPE" = "POWER9" ] ; then
-	make install T=ppc_64-power9-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC"
+	make install T=ppc_64-power9-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC" -j ${THREADS}
 elif [ "$CPUTYPE" = "POWER8" ] ; then
-	make install T=ppc_64-power8-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC"
+	make install T=ppc_64-power8-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC" -j ${THREADS}
 else
-	make install T=x86_64-native-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC"
+	make install T=x86_64-native-linuxapp-gcc DESTDIR=./build EXTRA_CFLAGS="-g -fPIC" -j ${THREADS}
 fi
-
+cd ..
