4566aee9c30f23ee5a664ceea5a9ef47be75aa45
Daniel Baker <daniel.n.baker@ibm.com>
2019-07-02 11:03:52 -0700

    spdk compilation.

--- src/lib/spdk/bootstrap.sh
+++ src/lib/spdk/bootstrap.sh
@@ -8,5 +8,18 @@ if [ ! -d ./spdk ] ; then
     git clone -b v18.10 https://github.com/spdk/spdk.git
 fi
 
-cd spdk ; ./configure --with-dpdk=$1/share/dpdk/x86_64-native-linuxapp-gcc/ --without-vhost --without-virtio
-make DPDK_DIR=$1/share/dpdk/x86_64-native-linuxapp-gcc/ CONFIG_RDMA=y
+CPUTYPE=$(lscpu | awk '/Model/{if($0 ~ "POWER") print($3)}')
+if [ "$CPUTYPE" = "POWER9" ] ; then
+    BTYPE="ppc_64-power9-linuxapp-gcc"
+elif [ "$CPUTYPE" = "POWER8" ] ; then
+    BTYPE="ppc_64-power8-linuxapp-gcc"
+else
+    BTYPE="x86_64-native-linuxapp-gcc"
+fi
+cd spdk ;
+cp ../save.patch .
+git apply save.patch
+echo ./configure --with-dpdk=../../dpdk/dpdk/build/share/dpdk/${BTYPE} --without-vhost --without-virtio
+./configure --with-dpdk=../../dpdk/dpdk/build/share/dpdk/${BTYPE} --without-vhost --without-virtio
+DPDKDIR=`readlink -f ../../dpdk/dpdk/build/share/dpdk/$BTYPE`
+make DPDK_DIR=$DPDKDIR CONFIG_RDMA=y -j40
